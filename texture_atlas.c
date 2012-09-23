3fce433837422e79d4deeeac8c235a2f7cc5b75f
Willem Thiart <beandaddy@beandaddy-P5KR.(none)>
2012-09-23 22:25:02 +1200

    bug fix

--- texture_atlas.c
+++ texture_atlas.c
@@ -1,20 +1,31 @@
 /*
- * =====================================================================================
- *
- *       Filename:  r_texture_atlas.c
- *
- *    Description:  
- *
- *        Version:  1.0
- *        Created:  03/27/11 19:52:56
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  YOUR NAME (), 
- *        Company:  
- *
- * =====================================================================================
- */
+ 
+Copyright (c) 2011, Willem-Hendrik Thiart
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * The names of its contributors may not be used to endorse or promote
+      products derived from this software without specific prior written
+      permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL WILLEM-HENDRIK THIART BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,48 +39,33 @@ static arraylistf_t *__atlases = NULL;
 
 typedef struct texture_s texture_t;
 
-//static char *str = "found %d %d %d %d %s \n";
+static char *str = "found %d %d %d %d\n";
 
-struct texture_s
-{
+struct texture_s {
     texture_t *kids[2];
     rect_t rect;
     int id;
 };
 
-typedef struct
-{
+typedef struct {
     int glimage;
     hashmap_t *textures;
     texture_t *root;
     int ntextures;
 
-    void (
-    *write_pixels_to_texture_cb
-    )    (
-    const void *pixels,
-    const rect_t * rect,
-    const unsigned int texture
-    );
+    void (*write_pixels_to_texture_cb) (const void *pixels,
+					const rect_t * rect,
+					const unsigned int texture);
 
-    int (
-    *create_texture_cb
-    )   (
-    const int w,
-    const int h
-    );
+    int (*create_texture_cb) (const int w, const int h);
 } atlas_t;
 
-static atlas_t *__get_atlas_from_id(
-    int id
-)
+static atlas_t *__get_atlas_from_id(int id)
 {
     return (atlas_t *) arraylistf_get(__atlases, id);
 }
 
-static unsigned long __ulong_hash(
-    const void *e1
-)
+static unsigned long __ulong_hash(const void *e1)
 {
     const long i1 = (unsigned long) e1;
 
@@ -77,10 +73,7 @@ static unsigned long __ulong_hash(
     return i1;
 }
 
-static long __ulong_compare(
-    const void *e1,
-    const void *e2
-)
+static long __ulong_compare(const void *e1, const void *e2)
 {
     const long i1 = (unsigned long) e1, i2 = (unsigned long) e2;
 
@@ -92,14 +85,15 @@ static long __ulong_compare(
  * @param width The size of this atlas
  * @param write_pixels_to_texture Callback for writing pixels to the atlas
  * @param create_texture_cb Callback for creating a new texture */
-int ren_texture_atlas_init(
-    int width,
-    void (*write_pixels_to_texture) (const void *pixels,
-                                     const rect_t * rect,
-                                     const unsigned int texture),
-    int (*create_texture_cb) (const int w,
-                              const int h)
-)
+int ren_texture_atlas_init(int width,
+			   void (*write_pixels_to_texture) (const void
+							    *pixels,
+							    const rect_t *
+							    rect,
+							    const unsigned
+							    int texture),
+			   int (*create_texture_cb) (const int w,
+						     const int h))
 {
     atlas_t *at;
 
@@ -107,122 +101,119 @@ int ren_texture_atlas_init(
 
     if (!__atlases)
     {
-        __atlases = arraylistf_new();
+	__atlases = arraylistf_new();
     }
 
     at = calloc(1, sizeof(atlas_t));
     tex = at->root = calloc(1, sizeof(texture_t));
     tex->rect.x = 0;
     tex->rect.y = 0;
-    tex->rect.w = width;        //1 << 8;
-    tex->rect.h = width;        //1 << 8;
+    tex->rect.w = width;	//1 << 8;
+    tex->rect.h = width;	//1 << 8;
     at->textures = hashmap_new(__ulong_hash, __ulong_compare);
     at->write_pixels_to_texture_cb = write_pixels_to_texture;
     at->create_texture_cb = create_texture_cb;
     if (at->create_texture_cb)
     {
-        at->glimage = at->create_texture_cb(tex->rect.w, tex->rect.h);
+	at->glimage = at->create_texture_cb(tex->rect.w, tex->rect.h);
     }
     return arraylistf_add(__atlases, at);
 }
 
 /*----------------------------------------------------------------------------*/
 
-#if 0
-static void __print(
-    texture_t * tex,
-    int depth
-)
+static void __print(texture_t * tex, int depth)
 {
+#if 0
     if (!tex)
-        return;
+	return;
     int ii;
 
     for (ii = 0; ii < depth; ii++)
-        printf(" ");
+	printf(" ");
     printf
-        ("%d,%d,%d,%d %d\n",
-         tex->rect.x, tex->rect.y, tex->rect.w, tex->rect.h, tex->id);
+	("%d,%d,%d,%d %d\n",
+	 tex->rect.x, tex->rect.y, tex->rect.w, tex->rect.h, tex->id);
     __print(tex->kids[0], depth + 1);
     __print(tex->kids[1], depth + 1);
-}
 #endif
+}
 
-static texture_t *__insert(
-    texture_t * tex,
-    int w,
-    int h,
-    int id
-)
+static texture_t *__insert(texture_t * tex,
+			   const int w, const int h, const int id)
 {
     assert(tex);
 
     /* not leaf */
     if (tex->kids[0])
     {
-        texture_t *new;
+	texture_t *new;
 
-        if ((new = __insert(tex->kids[0], w, h, id)))
-        {
-            return new;
-        }
-        else
-        {
-            return __insert(tex->kids[1], w, h, id);
-        }
+	/* insert at this branch... */
+	if ((new = __insert(tex->kids[0], w, h, id)))
+	{
+	    return new;
+	}
+	/* ...since that branch didn't work, try the other branch */
+	else
+	{
+	    return __insert(tex->kids[1], w, h, id);
+	}
     }
+    /* is a leaf */
     else
     {
-        /*  fits perfectly */
-        if (tex->rect.w == w && tex->rect.h == h)
-        {
-            tex->id = id;
-            assert(!tex->kids[0]);
-            assert(!tex->kids[1]);
-            return tex;
-        }
-        /*  doesn't fit */
-        else if (tex->rect.w < w || tex->rect.h < h || tex->id != 0)
-        {
-//            printf("doesn't fit, %d %d %d %d\n", tex->rect.w, w, tex->rect.h, h);
-//            assert(0);
-            return NULL;
-        }
-        else
-        {
-            assert(!tex->kids[0]);
-            assert(!tex->kids[1]);
-            tex->kids[0] = calloc(1, sizeof(texture_t));
-            tex->kids[1] = calloc(1, sizeof(texture_t));
-            int dw, dh;
+	/*  fits perfectly.. */
+	if (tex->rect.w == w && tex->rect.h == h &&
+	    /* ..and isn't in-use */
+	    tex->id == 0)
+	{
+	    tex->id = id;
+	    assert(!tex->kids[0]);
+	    assert(!tex->kids[1]);
+	    return tex;
+	}
+	/*  doesn't fit */
+	else if (tex->rect.w < w || tex->rect.h < h || tex->id != 0)
+	{
+	    return NULL;
+	}
+	else
+	{
+	    int dw, dh;
 
-            dw = tex->rect.w - w;
-            dh = tex->rect.h - h;
-            if (dh < dw)
-            {
-                tex->kids[0]->rect.x = tex->rect.x;
-                tex->kids[0]->rect.y = tex->rect.y;
-                tex->kids[0]->rect.w = w;
-                tex->kids[0]->rect.h = tex->rect.h;
-                tex->kids[1]->rect.x = tex->rect.x + w;
-                tex->kids[1]->rect.y = tex->rect.y;
-                tex->kids[1]->rect.w = tex->rect.w - w;
-                tex->kids[1]->rect.h = tex->rect.h;
-            }
-            else
-            {
-                tex->kids[0]->rect.x = tex->rect.x;
-                tex->kids[0]->rect.y = tex->rect.y;
-                tex->kids[0]->rect.w = tex->rect.w;
-                tex->kids[0]->rect.h = h;
-                tex->kids[1]->rect.x = tex->rect.x;
-                tex->kids[1]->rect.y = tex->rect.y + h;
-                tex->kids[1]->rect.w = tex->rect.w;
-                tex->kids[1]->rect.h = tex->rect.h - h;
-            }
+	    assert(!tex->kids[0]);
+	    assert(!tex->kids[1]);
+	    tex->kids[0] = calloc(1, sizeof(texture_t));
+	    tex->kids[1] = calloc(1, sizeof(texture_t));
 
-            return __insert(tex->kids[0], w, h, id);
-        }
+	    dw = tex->rect.w - w;
+	    dh = tex->rect.h - h;
+	    if (dh < dw)
+	    {
+		tex->kids[0]->rect.x = tex->rect.x;
+		tex->kids[0]->rect.y = tex->rect.y;
+		tex->kids[0]->rect.w = w;
+		tex->kids[0]->rect.h = tex->rect.h;
+		tex->kids[1]->rect.x = tex->rect.x + w;
+		tex->kids[1]->rect.y = tex->rect.y;
+		tex->kids[1]->rect.w = tex->rect.w - w;
+		tex->kids[1]->rect.h = tex->rect.h;
+	    }
+	    else
+	    {
+		tex->kids[0]->rect.x = tex->rect.x;
+		tex->kids[0]->rect.y = tex->rect.y;
+		tex->kids[0]->rect.w = tex->rect.w;
+		tex->kids[0]->rect.h = h;
+		tex->kids[1]->rect.x = tex->rect.x;
+		tex->kids[1]->rect.y = tex->rect.y + h;
+		tex->kids[1]->rect.w = tex->rect.w;
+		tex->kids[1]->rect.h = tex->rect.h - h;
+	    }
+
+	    return __insert(tex->kids[0], w, h, id);
+	}
     }
 }
 
@@ -230,12 +221,8 @@ static texture_t *__insert(
  * Write pixels to atlas, return the 'virtual texture id'
  * @return 'virtual texture id'
  */
-int ren_texture_atlas_push_pixels(
-    int id,
-    const void *pixel_data,
-    int w,
-    int h
-)
+int ren_texture_atlas_push_pixels(int id,
+				  const void *pixel_data, int w, int h)
 {
     texture_t *tex;
 
@@ -248,23 +235,22 @@ int ren_texture_atlas_push_pixels(
     assert(at);
 
     new_id = at->ntextures + 1;
-//      __print(at->root,0);
+    __print(at->root, 0);
 
+    /* insert into atlas */
     if (!(tex = __insert(at->root, w, h, new_id)))
     {
-//        printf("(%d, %d) \n", w, h);
-//        printf("(%d, %d) \n", at->root->rect.w, at->root->rect.h);
-        assert(false);
-        return 0;
+	assert(false);
+	return 0;
     }
 
     hashmap_put(at->textures, (void *) new_id, (void *) tex);
 
-//    printf(str, tex->rect.x, tex->rect.y, tex->rect.w, tex->rect.h, fname);
-
+    /* write pixels to texture */
     if (at->write_pixels_to_texture_cb)
     {
-        at->write_pixels_to_texture_cb(pixel_data, &tex->rect, at->glimage);
+	at->write_pixels_to_texture_cb(pixel_data, &tex->rect,
+				       at->glimage);
     }
 
     at->ntextures += 1;
@@ -275,10 +261,7 @@ int ren_texture_atlas_push_pixels(
 /**
  * Remove texture from atlas using filename
  */
-void ren_texture_atlas_remove_file(
-    int id,
-    const char *fname
-)
+void ren_texture_atlas_remove_file(int id, const char *fname)
 {
     atlas_t *at;
 
@@ -289,20 +272,14 @@ void ren_texture_atlas_remove_file(
 
 /**
  * Remove texture based off texture ID */
-void ren_texture_atlas_remove_texid(
-    int id,
-    const unsigned long texid
-)
+void ren_texture_atlas_remove_texid(int id, const unsigned long texid)
 {
     assert(false);
 }
 
 /**
  * @return true if atlas contains texture id, otherwise false */
-int ren_texture_atlas_contains_texid(
-    int id,
-    const unsigned long texid
-)
+int ren_texture_atlas_contains_texid(int id, const unsigned long texid)
 {
     atlas_t *at;
 
@@ -315,12 +292,9 @@ int ren_texture_atlas_contains_texid(
 /**
  * Get coordiantes using texture id
  */
-int ren_texture_atlas_get_coords_from_texid(
-    const int id,
-    const unsigned long texid,
-    vec2_t begin,
-    vec2_t end
-)
+int ren_texture_atlas_get_coords_from_texid(const int id,
+					    const unsigned long texid,
+					    vec2_t begin, vec2_t end)
 {
     atlas_t *at;
 
@@ -333,7 +307,7 @@ int ren_texture_atlas_get_coords_from_texid(
     tex = hashmap_get(at->textures, (void *) texid);
 
     vec2Set(begin, (float) tex->rect.x / at->root->rect.h,
-            (float) tex->rect.y / at->root->rect.h);
+	    (float) tex->rect.y / at->root->rect.h);
     vec2Copy(begin, end);
     end[0] += (float) tex->rect.w / at->root->rect.w;
     end[1] += (float) tex->rect.h / at->root->rect.h;
@@ -342,9 +316,7 @@ int ren_texture_atlas_get_coords_from_texid(
 
 /**
  * @return texture id */
-int ren_texture_atlas_get_texture(
-    const int id
-)
+int ren_texture_atlas_get_texture(const int id)
 {
     atlas_t *at;
 
@@ -355,9 +327,7 @@ int ren_texture_atlas_get_texture(
 
 /**
  * @return number of textures */
-int ren_texture_atlas_get_ntextures(
-    const int id
-)
+int ren_texture_atlas_get_ntextures(const int id)
 {
     atlas_t *at;
 
